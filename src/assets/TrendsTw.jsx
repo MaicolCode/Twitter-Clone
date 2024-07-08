261868db1f5d671bf0ea0fe620dc07df23bda8f7
MaicolCode <paucarjhon07@outlook.com>
2024-07-07 23:07:56 -0500

    Creacion de la modal en la card de trends.

--- src/assets/TrendsTw.jsx
+++ src/assets/TrendsTw.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
+
 // eslint-disable-next-line react/prop-types
 export function TwitterTrends({name, children, posts}){
+    const [viewModal, setViewModal] = useState(false)
+    const modalView = viewModal ? 'viewModal' : ''
 
+    function activeModal() {
+        setViewModal(!viewModal)
+    }   
+
+    if(viewModal){
+        document.querySelector('.tw-TrendsCard').addEventListener('click',activeModal)
+    }
     return(
         <section className="card-trend">
             <header className="tw-headTrends">
@@ -8,9 +19,20 @@ export function TwitterTrends({name, children, posts}){
                 <strong>{name}</strong>
                 <span>{posts ?`${posts} posts`: ''}</span>
             </header>
-            <button className="tw-buttonTrends">
+            <button className="tw-buttonTrends" onClick={activeModal}>
                 <ion-icon name="ellipsis-horizontal-outline" style={{fontSize:"16px"}}></ion-icon>
             </button>
+            <article className={`tw-modalTrends ${modalView}`} >
+                <InfoModal>No estoy interesado</InfoModal>
+                <InfoModal>El contenido es spam</InfoModal>
+            </article>
         </section>
+    )
+}
+
+// eslint-disable-next-line react/prop-types
+function InfoModal({children}){
+    return (
+        <strong className="tw-infoModal"><ion-icon name="sad-outline" style={{fontSize:"20px"}}></ion-icon> {children}</strong>
     )
 }
